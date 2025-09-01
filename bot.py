4474c101652a894c72d0c16df0baa7166f19b9c4
etudiantcisse <babacar.cisse2@ism.edu.sn>
2025-09-01 14:29:01 +0000

    Update API key and model to qwen3-235b-a22b-instruct-2507

--- bot.py
+++ bot.py
@@ -3,20 +3,20 @@ from openai import OpenAI
 
 client = OpenAI(
     base_url = "https://api.scaleway.ai/5378dadd-4f88-4884-ba7b-15f0f386168b/v1",
-    api_key = "34bf922e-9fb3-4c70-b116-a5ffdf516e00"
+    api_key = "26f351de-2e15-4eb1-93d6-74f8ead4b565"
 )
 
 
 def stream_response(user_message: str):
     response = client.chat.completions.create(
-        model="gpt-oss-120b",
+        model="qwen3-235b-a22b-instruct-2507",
         messages=[
             { "role": "system", "content": "You are a helpful assistant" },
             { "role": "user", "content": user_message },
         ],
         max_tokens=512,
-        temperature=1.2,
-        top_p=1,
+        temperature=0.7,
+        top_p=0.8,
         presence_penalty=0,
         stream=False,
     )
